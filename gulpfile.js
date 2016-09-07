220a15248691814b09f5170b63f25133c4d981bc
Maciej Szmida <maciek.bmg@gmail.com>
2016-09-07 21:47:59 +0200

    watch tasks added + compiling underscore templates

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,8 @@ const gulp = require("gulp"),
     del = require("del"),
     changed = require("gulp-changed"),
     browserify = require("browserify"),
+    watchify = require('watchify'),
+    jstify = require("jstify"),
     source = require("vinyl-source-stream"),
     buffer = require('vinyl-buffer'),
     uglify = require('gulp-uglify'),
@@ -13,14 +15,39 @@ const gulp = require("gulp"),
     runSequence = require('run-sequence');
 
 
-const src = ["src/app/*.html"],
-    dest = "dist/";
+const indexPath = "./src/app/index.html",
+    mainPath = "./src/app/js/main.js",
+    distPath = "./dist/";
+
+
+function makeBundle(browserify) {
+    return browserify.bundle()
+        .on("error", gutil.log)
+        // conversion from browserify text stream into vinyl stream
+        .pipe(source("main.js"))
+        // conversion from vinyl stream into vinyl buffer
+        .pipe(buffer())
+        // piping stream further
+        .pipe(uglify())
+        .pipe(rename("main.min.js"))
+        .pipe(size())
+        .pipe(gulp.dest(distPath + "js/"))
+            .on("end", function () {
+                gutil.log("Building JavaScript bundle has been completed!\n");
+            });
+}
+
+function copy(source, destination) {
+    return gulp.src(source)
+        .pipe(changed(destination))
+        .pipe(gulp.dest(destination));
+}
 
 
 gulp.task("default", ["assembly"]);
 
 gulp.task("assembly", function(callback) {
-    runSequence("clean", "copy", "build-js", function(err) {
+    runSequence("clean", "copy-index", "build-js", function(err) {
         if (err) {
             var exitCode = 1;
             gutil.log("[ERROR] gulp build task failed", err);
@@ -36,32 +63,56 @@ gulp.task("assembly", function(callback) {
 });
 
 gulp.task("clean", function() {
-    return del(dest).then(paths => {
+    return del(distPath).then(paths => {
         gutil.log("Cleaning has been completed!");
     });
 });
 
-gulp.task("copy", function() {
-    return gulp.src(src)
-        .pipe(changed(dest))
-        .pipe(gulp.dest(dest))
-        .on("end", function () { gutil.log("Copying files has been completed!"); });
+gulp.task("copy-index", function() {
+    return copy(indexPath, distPath)
+        .on("end", function () {
+            gutil.log("Index file has been updated!");
+        });
 });
 
 gulp.task("build-js", function() {
-    return browserify({
-            entries: "./src/app/js/app.js",
+    var b = browserify({
+            entries: [mainPath],
             debug: true
+        });
+
+    // jstify is a Browserify transform for creating modules of pre-compiled Underscore templates
+    b.transform(jstify);
+
+    return makeBundle(b);
+});
+
+gulp.task("watch", ["watch-html", "watch-js"]);
+
+gulp.task("watch-html", function() {
+    gulp.watch(indexPath, ["copy-index"]);
+});
+
+gulp.task("watch-js", function() {
+    var b = browserify({
+            entries: [mainPath],
+            debug: true,
+            cache: {},
+            packageCache: {},
+        });
+
+    b.plugin(watchify, {
+        ignoreWatch: ["**/node_modules/**"]
+    })
+        .on("update", function () {
+            return makeBundle(b);
         })
-        .bundle()
-        .on("error", gutil.log)
-        // conversion from build to stream
-        .pipe(source("app.js"))
-        .pipe(buffer())
-        // piping stream to tasks
-        .pipe(uglify())
-        .pipe(rename("app.min.js"))
-        .pipe(size())
-        .pipe(gulp.dest(dest + "js/"))
-        .on("end", function () { gutil.log("Building JavaScript has been completed!"); });
+        .on("log", function (msg) {
+            gutil.log(msg);
+        });
+
+    // jstify is a Browserify transform for creating modules of pre-compiled Underscore templates
+    b.transform(jstify);
+
+    return makeBundle(b);
 });
